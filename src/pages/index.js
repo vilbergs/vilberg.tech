2059fe5c51bbc755725d55d5feeeac19bfb4c33d
Vilberg Eliasson <vilbergs@gmail.com>
2019-09-09 20:45:08 +0200

    Set up landing page and some routing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,93 @@
-import React from 'react'
-import Layout from '../components/layout'
-import styled from '@emotion/styled'
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core'
+import { Link } from 'gatsby'
+import 'typeface-bungee-shade'
+import 'typeface-share-tech-mono'
 
-const Body = styled.div`
+const container = css`
   display: grid;
-  grid-gap: 15px;
-  grid-template-columns: repeat(2, 1fr);
+  height: 100vh;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: 2fr 5em 1fr 2fr;
 
-  grid-template-areas: 'i p';
+  @media (min-width: 43.75em) {
+    grid-template-rows: 2fr 10em 1fr 2fr;
+  }
+`
+
+const landingHeading = css`
+  grid-column: 2;
+  grid-row: 2;
+  font-size: 3.5em;
+  font-family: Bungee Shade;
+  color: #ff3900;
+  padding: 5px;
+  margin: 0;
+  @media (min-width: 43.75em) {
+    font-size: 8em;
+    /* 2.5x body copy size = 40px */
+    line-height: 1.125;
+    padding: 5px;
+  }
+`
+const navigation = css`
+  grid-column: 2;
+  grid-row: 3;
+
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    flex-direction: row;
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+
+    li {
+      font-family: Share Tech Mono;
+      color: #ff3900;
+      flex-grow: 1;
+      font-size: 1.9em;
+      font-weight: 700;
+      text-align: center;
+      margin: 0.25em 0;
+      padding: 0 0.4em;
+      border-left: 2px solid #ff3900;
+
+      @media (min-width: 43.75em) {
+        font-size: 4em;
+        /* 2.5x body copy size = 40px */
+        border-left: 7px solid #ff3900;
+      }
+
+      &:first-of-type {
+        border-left: none;
+      }
+
+      a {
+        color: inherit;
+      }
+    }
+  }
 `
 
 const IndexPage = () => (
-  <Layout>
-    <Body>Hey</Body>
-  </Layout>
+  <div css={container}>
+    <h1 css={landingHeading}>Vilberg </h1>
+    <nav css={navigation}>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/blog">Blog</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  </div>
 )
 
 export default IndexPage
--- src/posts/2019-08-18/index.md
+++ src/posts/2019-08-18/index.md
@@ -1,6 +1,7 @@
 ---
 path: '/my-first-post'
 title: 'My First Post!'
+date: '2019-08-18'
 featuredImage: './pancakes.jpg'
 mobileImage: './pancakes_mobile.jpg'
 ---
