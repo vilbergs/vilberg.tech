7615dedea2a4f8ff3a560acf34c59e6f11462348
Vilberg Eliasson <vilbergs@gmail.com>
2019-09-18 19:00:03 +0200

    lgogo component

---
+++ src/components/Logo.js
@@ -0,0 +1,27 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core'
+
+const logo = size => css`
+  font-family: Bungee Shade;
+  font-size: ${size}em;
+  color: rgb(0, 0, 0, 0.87);
+  margin: 0;
+
+  user-select: none;
+
+  @media (min-width: 43.75em) {
+    font-size: ${size * 2}em;
+    /* 2.5x body copy size = 40px */
+    line-height: 1.125;
+  }
+`
+
+const Logo = props => {
+  return (
+    <h1 css={logo(props.size)} {...props}>
+      Vilberg
+    </h1>
+  )
+}
+
+export default Logo
