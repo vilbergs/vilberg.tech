b87db455bfa00fe0bb40364cb59afce7e01092d6
Vilberg <vilbergs@gmail.com>
2020-01-30 07:31:31 +0100

    More portfolio work

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -4,6 +4,7 @@ import { useStaticQuery, graphql } from 'gatsby'
 import gcd from 'gcd'
 import Layout from '../components/layout'
 import Img from 'gatsby-image'
+import BackgroundImage from 'gatsby-background-image'
 
 const body = css`
   grid-column: 2 / 12;
@@ -16,11 +17,29 @@ const body = css`
 
 const gallery = css`
   display: grid;
-  position: relative;
-  grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
   grid-auto-flow: dense;
+  grid-auto-rows: 100px;
   grid-gap: 5px;
   .portrait {
+  }
+`
+
+const imageStyle = css`
+  object-fit: cover;
+  width: 100%;
+  max-height: 100%;
+`
+
+const overlay = image => css`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+
+  &:hover {
+    background-color: blue;
   }
 `
 
@@ -54,18 +73,49 @@ const Portfolio = () => {
         </p>
         <div css={gallery}>
           {data.portfolio.edges.map(image => (
+            // <div
+            // style={{
+            //   position: 'relative',
+            // ...spanByAspectRatio(
+            //   image.node.childImageSharp.fluid.presentationWidth,
+            //   image.node.childImageSharp.fluid.presentationHeight
+            // ),
+            // }}
+            // >
+            //   <Img
+
+            //     imgStyle={{ objectFit: 'cover' }}
+            //     key={image.node.id}
+            //     loading="lazy"
+            //     fluid={image.node.childImageSharp.fluid}
+            //   >
+            //
+            //   </Img>
+            // </div>
+
+            // <BackgroundImage
+            //   key={image.node.childImageSharp.id}
+            //   Tag="div"
+            //   fluid={image.node.childImageSharp.fluid}
+            //   css={spanByAspectRatio(
+            //     image.node.childImageSharp.fluid.presentationWidth,
+            //     image.node.childImageSharp.fluid.presentationHeight
+            //   )}
+            // >
+            //   <div css={overlay(image.node.childImageSharp.fluid)}>overlay</div>
+            // </BackgroundImage>
+
             <div
-              style={spanByAspectRatio(
+              css={spanByAspectRatio(
                 image.node.childImageSharp.fluid.presentationWidth,
                 image.node.childImageSharp.fluid.presentationHeight
               )}
             >
-              <Img
-                key={image.node.id}
-                loading="lazy"
-                fluid={image.node.childImageSharp.fluid}
-              />
-              <div>overlay</div>
+              <img
+                css={imageStyle}
+                src={image.node.childImageSharp.fluid.src}
+              ></img>
+              <div css={overlay}>overlay</div>
             </div>
           ))}
         </div>
@@ -86,17 +136,19 @@ function spanByAspectRatio(width, height) {
   let rows = height / greatest
 
   if (cols === rows) {
-    cols = Math.floor(Math.random() * 4)
-    rows = Math.floor(Math.random() * 4)
+    const rand = Math.floor(Math.random() * 4)
+    cols = rand
+    rows = rand
 
     cols = cols === 0 ? 1 : cols
     rows = rows === 0 ? 1 : rows
   }
 
-  return {
-    gridColumn: `span ${cols * Math.floor(Math.random() * 2)}`,
-    gridRow: `span ${rows * Math.floor(Math.random() * 2)}`,
-  }
+  return css`
+    position: relative;
+    grid-column: span ${cols};
+    grid-row: span ${rows};
+  `
 }
 
 export default Portfolio
