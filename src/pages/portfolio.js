b5b7c2cec19654454d1ba3e9656dec9e0acf314c
Vilberg Eliasson <vilbergs@gmail.com>
2020-02-03 19:19:13 +0100

    Use magic math to get a magical column layout

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -9,30 +9,20 @@ import BackgroundImage from 'gatsby-background-image'
 import Lightbox from 'react-image-lightbox'
 import 'react-image-lightbox/style.css' // This only needs to be imported once in your app
 
-const body = css`
+const IMAGE_WIDTH = 33.33333333333333
+
+const body = height => css`
   grid-column: 2 / 12;
-  grid-row: 2;
+  display: flex;
+  flex-flow: column wrap;
+  height: 36%; /* TODO: This number is magical, find out why */
 
   p {
     text-align: center;
   }
 `
 
-const gallery = css`
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
-  grid-gap: 5px;
-  grid-auto-flow: dense;
-  grid-auto-rows: 150px;
-`
-
-const imageStyle = css`
-  object-fit: cover;
-  width: 100%;
-  max-height: 100%;
-`
-
-const overlay = image => css`
+const overlay = css`
   position: absolute;
   top: 0;
   left: 0;
@@ -82,7 +72,7 @@ const Portfolio = () => {
     setLightboxOpen(open => !open)
   }
 
-  const handleLightboxClose = index => {
+  const handleLightboxClose = () => {
     setLightboxOpen(open => !open)
   }
 
@@ -90,57 +80,28 @@ const Portfolio = () => {
     data.portfolio.edges[index].node.childImageSharp.fluid.src
 
   const nextIndex = (activePhoto + 1) % data.portfolio.edges.length
+
   const previousIndex =
     (activePhoto + data.portfolio.edges.length - 1) %
     data.portfolio.edges.length
 
   return (
     <Layout>
-      <div css={body}>
-        <p>
-          This page is a work in progress, but please enjoy these shots while
-          I'm at it!
-        </p>
-        <div css={gallery}>
-          {data.portfolio.edges.map((image, index) => (
-            // <div
-            // style={{
-            //   position: 'relative',
-            // ...spanByAspectRatio(
-            //   image.node.childImageSharp.fluid.presentationWidth,
-            //   image.node.childImageSharp.fluid.presentationHeight
-            // ),
-            // }}
-            // >
-            //   <Img
-
-            //     imgStyle={{ objectFit: 'cover' }}
-            //     key={image.node.id}
-            //     loading="lazy"
-            //     fluid={image.node.childImageSharp.fluid}
-            //   >
-            //
-            //   </Img>
-            // </div>
-
-            <BackgroundImage
-              key={image.node.childImageSharp.id}
-              Tag="div"
-              fluid={image.node.childImageSharp.fluid}
-              css={spanByAspectRatio(
-                image.node.childImageSharp.fluid.presentationWidth,
-                image.node.childImageSharp.fluid.presentationHeight
-              )}
-            >
-              <div
-                onClick={() => handleLightboxOpen(index)}
-                css={overlay(image.node.childImageSharp.fluid)}
-              >
-                Open
-              </div>
-            </BackgroundImage>
-          ))}
-        </div>
+      <div css={body()}>
+        {data.portfolio.edges.map((image, index) => (
+          <BackgroundImage
+            key={image.node.childImageSharp.id}
+            Tag="div"
+            fluid={image.node.childImageSharp.fluid}
+            css={spanByAspectRatio(
+              image.node.childImageSharp.fluid.aspectRatio
+            )}
+          >
+            <div onClick={() => handleLightboxOpen(index)} css={overlay}>
+              Open
+            </div>
+          </BackgroundImage>
+        ))}
       </div>
       {lightboxOpen ? (
         <Lightbox
@@ -157,16 +118,14 @@ const Portfolio = () => {
   )
 }
 
-function spanByAspectRatio(width, height) {
-  const greatest = gcd(width, height)
-
-  let cols = width / greatest
-  let rows = height / greatest
-
+function spanByAspectRatio(ratio) {
   return css`
     position: relative;
-    grid-column: span ${cols};
-    grid-row: span ${rows};
+    flex: 0 0 auto;
+    display: flex;
+    width: ${IMAGE_WIDTH}%;
+    padding-top: ${IMAGE_WIDTH / ratio}%;
+    margin: 5px;
   `
 }
 
