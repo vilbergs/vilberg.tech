2a60e8e1430f24c416af622123f0da9263c9b01c
Vilberg Eliasson <vilbergs@gmail.com>
2020-02-18 19:55:19 +0100

    Fix site metadata and add sitemap plugin:x

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Vilberg`,
+    description: `Vilberg's Personal website, blog and portfolio`,
+    author: `Vilberg Sindri`,
+    siteUrl: `https://vilberg.tech`,
   },
   plugins: [
+    `gatsby-plugin-sitemap`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-react-helmet`,
