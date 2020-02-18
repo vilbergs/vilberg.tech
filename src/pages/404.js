65514ea7dc499d0ce2f53ddb203ee2efc23ef4b0
Vilberg Eliasson <vilbergs@gmail.com>
2020-02-18 20:12:49 +0100

    Fix styling for 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,21 @@
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core'
 import Layout from '../components/Layout'
+import Heading from '../components/Heading'
+import SubHeading from '../components/SubHeading'
 
 const body = css`
   grid-column: 4 / 10;
+
+  height: calc(100vh - 350px);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  h1 {
+    font-size: 10em;
+  }
 
   @media (max-width: 43.75em) {
     grid-column: 2 / 12;
@@ -13,8 +25,8 @@ const body = css`
 const NotFoundPage = () => (
   <Layout title="404: Not found">
     <main css={body}>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <Heading>404</Heading>
+      <SubHeading>Oh no! We're lost!</SubHeading>
     </main>
   </Layout>
 )
